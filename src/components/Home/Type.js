f22524f259ea443083e7c5394b9ff30a78a2fb64
Nima ferdowsi <nima.ferdowsi@sinansoft.com>
2023-09-16 17:59:03 +0330

    first

---
+++ src/components/Home/Type.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Developer",
+          "Frontend developer",
+          "backend developer",
+          "Mobile App developer",
+          "MERN Stack Developer",
+          
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
